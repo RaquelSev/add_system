1bde8842742d657e5f89325611f54cbafe9d29b6
Raquel Severo <raquel.sev.celia@gmail.com>
2023-03-12 03:09:04 +0100

    Texfield component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Banner from "./components/Banner/Banner.js"
+import Banner from "./components/Banner"
+import TextField from "./components/TextField";
 
 function App() {
   return (
     <div className="App">
-      <Banner></Banner>
+      <Banner />
+      <TextField label="Name" placeholder="Type your name"/>
+      <TextField label="Job" placeholder="Type your position"/>
+      <TextField label="Image" placeholder="Type the image address"/>
     </div>
   );
 }
---
+++ src/components/TextField/index.js
@@ -0,0 +1,15 @@
+import './TextField.css'
+
+const TextField = (props) => {
+
+    const placeholderModif = `${props.placeholder}...`;
+    
+    return (
+      <div className="text-field">
+        <label>{ props.label }</label>
+        <input placeholder={placeholderModif}/>
+      </div>
+    )
+}
+
+export default TextField;
