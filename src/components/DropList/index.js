3ef33aba849066ba5c81f8d1810cd1cd3d644805
Raquel Severo <raquel.sev.celia@gmail.com>
2023-03-12 20:30:16 +0100

    Button component

---
+++ src/components/DropList/index.js
@@ -0,0 +1,19 @@
+import './DropList.css';
+
+const DropList = (props) => {
+
+    return (
+        <div className='drop-list'>
+            <label>
+                {props.label}
+            </label>
+            <select>
+                {props.itens.map(item => 
+                    <option key={item}>{item}</option>)}
+            </select>
+        </div>
+    )
+
+}
+
+export default DropList;
