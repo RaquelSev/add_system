a6733b1e391c5e049dfcce28eb8280f082058e9d
Raquel Severo <raquel.sev.celia@gmail.com>
2023-03-12 20:49:09 +0100

    Understanding events

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -3,13 +3,23 @@ import './TextField.css'
 const TextField = (props) => {
 
     const placeholderModif = `${props.placeholder}...`;
+    let typeValue = '';
+
+    const whenTyped = (event) => {
+      typeValue = event.target.value;
+      console.log(typeValue)
+    }
     
     return (
       <div className="text-field">
         <label>
-            { props.label }
+            {props.label}
           </label>
-        <input required={props.mandatory} placeholder={placeholderModif}/>
+        <input
+          value={typeValue} 
+          onChange={whenTyped}
+          required={props.mandatory} 
+          placeholder={placeholderModif}/>
       </div>
     )
 }
